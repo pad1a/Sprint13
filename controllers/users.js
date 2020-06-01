6790b1ac7b3a92f205edf1acbe4966decefea2c9
Pavel Maslov <pavel.masslove@gmail.com>
2020-06-01 15:55:07 +0300

    Переделан контроллер createUser Создан контроллер login

--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,50 @@
 // controllers/users.js
 // это файл контроллеров
+
+const bcrypt = require('bcryptjs');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+const jwt = require('jsonwebtoken');
+const validator = require('validator');
 const User = require('../models/user');
 
-const createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        return res.status(400).send({ message: err.message });
-      }
-      if (err.name === 'CastError') {
-        return res.status(400).send({ message: err.message });
-      }
-      return res.status(500).send({ message: err.message });
-    });
+module.exports.createUser = (req, res) => {
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
+  if (password.length >= 8) {
+    bcrypt.hash(password, 10)
+      .then((hash) => User.create({
+        name,
+        about,
+        avatar,
+        email,
+        password: hash,
+      }))
+      .then((user) => {
+        if (validator.isURL(avatar)) {
+          res.status(201).send({
+            _id: user._id,
+            name: user.name,
+            about: user.about,
+            avatar: user.avatar,
+            email: user.email,
+          });
+        } else res.status(400).send({ message: 'Ошибка ссылки на изображение' });
+      })
+      .catch((err) => {
+        if (err.name === 'ValidationError') {
+          return res.status(400)
+            .send({ message: err.message });
+        }
+        if (err.name === 'CastError') {
+          return res.status(400)
+            .send({ message: err.message });
+        }
+        return res.status(500)
+          .send({ message: err.message });
+      });
+  } else res.status(400).send({ message: 'Пароль слишком короткий' });
 };
 
 const allUsers = (req, res) => {
@@ -50,4 +80,20 @@ const oneUser = (req, res) => {
     });
 };
 
-module.exports = { createUser, allUsers, oneUser };
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+  return User.findUserByEmail(email, password)
+    .then((user) => {
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        { expiresIn: '7d' },
+      );
+      res.send({ token });
+    })
+    .catch((err) => {
+      res.status(401).send({ message: err.message });
+    });
+};
+
+module.exports = { allUsers, oneUser };

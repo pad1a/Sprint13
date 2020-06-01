bca12fad4a7956399fba6d65634a31006a7b3ceb
Pavel Maslov <pavel.masslove@gmail.com>
2020-06-01 15:26:19 +0300

    Вынес роуты в отдельный файл Защитил роуты авторизацией Убрал лишний код

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-// const path = require('path');
 const express = require('express');
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const users = require('./routes/users');
-const cards = require('./routes/cards');
 
+const auth = require('./middlewares/auth');
+const { login, createUser } = require('./controllers/users');
+const routes = require('./routes/routes.js');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -16,33 +17,21 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
-// логирование
-/* const timeLog = (req, res, next) => {
-  console.log(new Date(), req.method);
-  next();
-};
-*/
-// app.use(timeLog);
-
 // статическая раздача
 // app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// временное решение авторизации:
-app.use((req, res, next) => {
-  req.user = {
-    _id: '5ec435607432699b7cba5fc1',
-  };
-  next();
-});
+app.post('/signin', login);
+app.post('/signup', createUser);
 
-app.use('/users', users);
-app.use('/cards', cards);
+// защитили все роуты кроме создания юзера и логина
+app.use(auth);
+app.use('/', routes);
+
 app.use((req, res) => res.status(404).send({ message: `Запрашиваемый ресурс: ${req.url} не найден` }));
 app.use((err, req, res) => res.status(500).json({ message: `Ошибка${err}` }));
 
 // слушаем сервер при каждом обращении
 app.listen(PORT, () => {
-  // console.log(`App listening on port ${PORT}`);
 });

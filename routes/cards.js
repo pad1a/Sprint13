b6863383ddbbd51652faeb44f38bf26dc94148af
Pavel Maslov <pavel.masslove@gmail.com>
2020-06-23 10:50:51 +0300

    Переписаны роуты routes, cards, users

--- routes/cards.js
+++ routes/cards.js
@@ -1,13 +1,42 @@
-const cardsRoute = require('express').Router();
-const { createCard, allCards, delCard } = require('../controllers/cards');
+// routes/cards.js
+const cardsRouter = require('express').Router();
+const { Joi, celebrate } = require('celebrate');
+const { urlValidate } = require('../middlewares/isURL');
+
+const {
+  getCards, createCard, deleteCard, likeCard, dislikeCard,
+} = require('../controllers/cards');
 
 // GET
-cardsRoute.get('/', allCards);
+cardsRouter.get('/', getCards);
 
 // POST
-cardsRoute.post('/', createCard);
+cardsRouter.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    // eslint-disable-next-line no-useless-escape
+    link: Joi.string().custom(urlValidate, 'urlValidator').required(),
+  }),
+}), createCard);
 
-// DELETE
-cardsRoute.delete('/:cardId', delCard);
+// DEL
+cardsRouter.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), deleteCard);
 
-module.exports = cardsRoute;
+cardsRouter.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), dislikeCard);
+
+// PUT
+cardsRouter.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), likeCard);
+
+module.exports = cardsRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
 // routes/users.js
-const usersRoute = require('express').Router();
-const { createUser, allUsers, oneUser } = require('../controllers/users');
+const usersRouter = require('express').Router();
+const { Joi, celebrate } = require('celebrate');
+const {
+  getUsers, findUser, updateUser, updateUserAvatar,
+} = require('../controllers/users');
+const { urlValidate } = require('../middlewares/isURL');
 
 // GET
-usersRoute.get('/', allUsers);
-usersRoute.get('/:userId', oneUser);
+usersRouter.get('/', getUsers);
+usersRouter.get('/:id', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().alphanum().length(24),
+  }),
+}), findUser);
 
-module.exports = usersRoute;
+// PATCH
+usersRouter.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), updateUser);
+
+usersRouter.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    // eslint-disable-next-line no-useless-escape
+    avatar: Joi.string().custom(urlValidate, 'urlValidator').required(),
+  }),
+}), updateUserAvatar);
+
+module.exports = usersRouter;

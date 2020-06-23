498fa9c60700ea47e299479936a5617f7dd0a7a9
Pavel Maslov <pavel.masslove@gmail.com>
2020-06-23 09:39:14 +0300

    Sprint 15 Start - установлены новые модули - подготовлен app.js

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
+const cookieParser = require('cookie-parser');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const express = require('express');
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
 const auth = require('./middlewares/auth');
-const { login, createUser } = require('./controllers/users');
 const routes = require('./routes/routes.js');
+const { Joi, celebrate, errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { ErrorMiddleware } = require('./middlewares/error');
+const { urlValidate } = require('./middlewares/isURL');
+const { login, createUser } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -21,17 +32,46 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 // app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.use(limiter);
+app.use(helmet());
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Падение сервера');
+  }, 0);
+});
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+    // eslint-disable-next-line no-useless-escape
+    avatar: Joi.string().custom(urlValidate, 'urlValidator').required(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), createUser);
 
 // защитили все роуты кроме создания юзера и логина
 app.use(auth);
 app.use('/', routes);
 
-app.use((req, res) => res.status(404).send({ message: `Запрашиваемый ресурс: ${req.url} не найден` }));
-app.use((err, req, res) => res.status(500).json({ message: `Ошибка${err}` }));
+app.use(errorLogger);
+app.use(errors());
+app.use(ErrorMiddleware);
+
 
 // слушаем сервер при каждом обращении
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`App listening on port ${PORT}`);
 });
